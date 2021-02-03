34df20d0f789fc9c98da46b71f8a1b60aeae06b0
Sayeda-e <sayeda.mosawi786@gmail.com>
2021-02-04 00:56:35 +0430

    first commit

---
+++ include/header.php
@@ -0,0 +1,36 @@
+  <!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Bootstrap 101 Template</title>
+
+    <!-- Bootstrap -->
+    <link href="boot/css/bootstrap.css" rel="stylesheet">
+    <link href="boot/css/mycss.css" rel="stylesheet">
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="boot/js/html5shiv.min.js"></script>
+      <script src="boot/js/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+  
+  
+    <div class="container-fluid">
+	<!--row-->
+	   <div class="row" id="top">
+	        <div class="col-md-3" id="logo">
+			 <img class="img-responsive" src="img/logo.png" alt="سمبول">
+			</div>
+			 <div class="col-md-6">
+			 
+			</div>
+			 <div class="col-md-3">
+			<h1 id="dept-name"> خریداری اجناس کمپیوتری</h1>
+			</div>
+	    </div>
+		<br/>
